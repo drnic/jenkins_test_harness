cbdf9865ee8957468837f4fd1d9bef79e5463eb0
Dr Nic Williams <drnicwilliams@gmail.com>
2013-04-23 15:23:50 -0700

    initial scaffold

---
+++ lib/jenkins/test/harness.rb
@@ -0,0 +1,9 @@
+require "jenkins/test/harness/version"
+
+module Jenkins
+  module Test
+    module Harness
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/jenkins/test/harness/version.rb
@@ -0,0 +1,7 @@
+module Jenkins
+  module Test
+    module Harness
+      VERSION = "0.0.1"
+    end
+  end
+end
